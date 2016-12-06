3dfd0ab3352aa2a6dfe64d1fea568a9e5441a006
nahuel <nahuelolgiati@hotmail.com>
2016-12-05 22:59:31 -0300

    first video

--- assets/js/vitality.js
+++ assets/js/vitality.js
@@ -4,12 +4,12 @@
  * To use this theme you must have a license purchased at WrapBootstrap (https://wrapbootstrap.com)
  */
 
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth Scrolling: Smooth scrolls to an ID on the current page.
     // To use this feature, add a link on your page that links to an ID, and add the .page-scroll class to the link itself. See the docs for more details.
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -18,16 +18,16 @@
     });
 
     // Activates floating label headings for the contact form.
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
         $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
+    }).on("focus", ".floating-label-form-group", function () {
         $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
+    }).on("blur", ".floating-label-form-group", function () {
         $(this).removeClass("floating-label-form-group-with-focus");
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
@@ -97,9 +97,24 @@
         offset: 51
     })
 
+    // Portfolio parameters
+    $('.portfolio').bind('click', function (event) {
+        var title = $(this).data('title');
+        var videoid = $(this).data('videoid');
+        $('#portfolioVideoTitle').text(title);
+        $('#portfolioVideoIframe').attr('src', 'https://www.youtube.com/embed/'+videoid);
+    });
+
+    // Portfolio Close
+    $('#portfolioVideoClose').bind('click', function (event) {
+        var url = $('#portfolioVideoIframe').attr('src');
+        $('#portfolioVideoIframe').attr('src', '');
+        $('#portfolioVideoIframe').attr('src', url);
+    });
+
     // Portfolio Filtering Scripts & Hover Effect
     var filterList = {
-        init: function() {
+        init: function () {
 
             // MixItUp plugin
             // http://mixitup.io
@@ -114,11 +129,11 @@
 
         },
 
-        hoverEffect: function() {
+        hoverEffect: function () {
 
             // Simple parallax effect
             $('#portfoliolist .portfolio').hover(
-                function() {
+                function () {
                     $(this).find('.caption').stop().animate({
                         bottom: 0
                     }, 200, 'easeOutQuad');
@@ -126,7 +141,7 @@
                         top: -20
                     }, 300, 'easeOutQuad');
                 },
-                function() {
+                function () {
                     $(this).find('.caption').stop().animate({
                         bottom: -75
                     }, 200, 'easeInQuad');
@@ -144,9 +159,10 @@
 
 })(jQuery); // End of use strict
 
-// Load WOW.js on non-touch devices
 var isPhoneDevice = "ontouchstart" in document.documentElement;
-$(document).ready(function() {
+$(document).ready(function () {
+
+    // Load WOW.js on non-touch devices
     if (isPhoneDevice) {
         //mobile
     } else {
@@ -157,4 +173,10 @@ $(document).ready(function() {
         })
         wow.init();
     }
+
+    // Portfolio thumbnail
+    $('#portfoliolist .portfolio').each(function (i, val) {
+        var videoid = $(val).data('videoid');
+        $(val).find(".portfolio-thumbnail").attr('src', 'http://img.youtube.com/vi/'+videoid+'/0.jpg');
+    });
 });
