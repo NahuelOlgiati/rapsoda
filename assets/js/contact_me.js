94dcca6d5638aababef6ddf66c0b361dd14f7f6c
nahuel <nahuelolgiati@hotmail.com>
2016-12-15 16:07:51 -0300

    rapsoda mail

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/nahuel_gta2@hotmail.com",
+                url: "https://formspree.io/bohemianrapsoda@hotmail.com",
                 type: "POST",
                 data: {
                     name: name,
@@ -34,7 +34,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Tu mensaje se ha enviado con exito!. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
